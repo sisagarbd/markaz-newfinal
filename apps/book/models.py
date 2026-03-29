087399df416b3f69e27b81e74221503f547d62e8
Saiful Islam Sagar <46032500+sisagarbd@users.noreply.github.com>
2026-03-29 23:24:21 +0600

    book add

--- apps/book/models.py
+++ apps/book/models.py
@@ -55,39 +55,64 @@ class Publisher(models.Model):
 
 class Book(models.Model):
 
-    STATUS = [
-        ("draft", "Draft"),
-        ("published", "Published"),
-        ("borrowed", "Borrowed"),
-        ("reserved", "Reserved"),
-        ("lost", "Lost"),
-    ]
-
+    # Basic Information
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     B_title = models.CharField(max_length=200,  default="Untitled")
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
-    language = models.CharField(max_length=100, blank=True)
-    library = models.ForeignKey(LibraryBranch, on_delete=models.SET_NULL, null=True)
-    total_volumes = models.PositiveIntegerField(null=True, blank=True)
-    status = models.CharField(max_length=20, choices=STATUS, default="draft")
-    # book_type = models.CharField(max_length=100, blank=True)
-    # edition = models.CharField(max_length=100, blank=True)
-    # publisher = models.ForeignKey(Publisher, on_delete=models.SET_NULL, null=True)
-    # publishing_year = models.PositiveIntegerField(null=True, blank=True)
-    # isbn = models.CharField(max_length=20, blank=True)
-    # pages = models.PositiveIntegerField(null=True, blank=True)
-    # catagory = models.ForeignKey(Catagory, on_delete=models.SET_NULL, null=True)
-    # ddc_no = models.CharField(max_length=20, blank=True)
-    # tags = models.ManyToManyField(Tag, blank=True)
-    # added_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    # notes = models.TextField(blank=True)
-    # shelf_no = models.CharField(max_length=50, blank=True, null=True)
+    isbn = models.CharField(max_length=50, blank=True, unique=True)
+    call_no = models.CharField(max_length=50, blank=True, unique=True)
+    year = models.IntegerField(blank=True, null=True)
+    ddc_no = models.CharField(max_length=50, blank=True)
+    Description = models.TextField(blank=True, null=True)
+
+    #Foreign Keys and Relations
+    catagory = models.ForeignKey(Catagory, on_delete=models.SET_NULL, null=True, blank=True)
+    publisher = models.ForeignKey(Publisher, on_delete=models.SET_NULL, null=True, blank=True)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, blank=True)
+    library = models.ForeignKey(LibraryBranch, on_delete=models.SET_NULL, null=True, blank=True)
 
 
+    # Status Info
+    volume = models.IntegerField(default=1)
+    pages = models.IntegerField(blank=True, null=True)
+    edition = models.IntegerField(blank=True, null=True)
 
+    # Toocle Fields
+
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.B_title
+
+
+
+
+
+
+
+
+    # author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    # language = models.CharField(max_length=100, blank=True)
+    # library = models.ForeignKey(LibraryBranch, on_delete=models.SET_NULL, null=True)
+    # total_volumes = models.PositiveIntegerField(null=True, blank=True)
+    # status = models.CharField(max_length=20, choices=STATUS, default="draft")
+    # # book_type = models.CharField(max_length=100, blank=True)
+    # # edition = models.CharField(max_length=100, blank=True)
+    # # publisher = models.ForeignKey(Publisher, on_delete=models.SET_NULL, null=True)
+    # # publishing_year = models.PositiveIntegerField(null=True, blank=True)
+    # # isbn = models.CharField(max_length=20, blank=True)
+    # # pages = models.PositiveIntegerField(null=True, blank=True)
+    # # catagory = models.ForeignKey(Catagory, on_delete=models.SET_NULL, null=True)
+    # # ddc_no = models.CharField(max_length=20, blank=True)
+    # # tags = models.ManyToManyField(Tag, blank=True)
+    # # added_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    # # notes = models.TextField(blank=True)
+    # # shelf_no = models.CharField(max_length=50, blank=True, null=True)
+
+
+
+
+    # def __str__(self):
+    #     return self.B_title
 
 
 # Create your models here.
